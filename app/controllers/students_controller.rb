ec45f90c434d3d27701c5049b0e2cd415823a38f
Evalyne <evalyne.mueke@student.moringaschool.com>
2023-03-13 21:52:19 +0300

    Highest grade route

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,4 +7,8 @@ class StudentsController < ApplicationController
         students = Student.order(grade: :desc)
         render json: students
     end
+    def highest_grade
+        highest_grade_student = Student.order(grade: :desc).first
+        render json: highest_grade_student
+    end
 end
